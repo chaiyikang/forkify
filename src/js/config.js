9eb474d4fc64d534f1f7efc95887d03ff726eca4
chaiyikang <chaiyikang52@gmail.com>
2023-06-18 16:18:11 +0800

    first commit of branch

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
 export const KEY = 'afa1eee4-2f96-4ab9-b455-df329c5150dd';
 export const MODAL_CLOSE_SEC = 2.5;
+export const HELLO = 500;
